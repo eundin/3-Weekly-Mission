b20345f92efc8c5dd450ab9b231fd844475a889c
eundin <escience1202@gmail.com>
2024-01-01 21:43:43 +0900

    feat:folder 페이지 기본 레이아웃 완성

--- src/user-profile.js
+++ src/user-profile.js
@@ -1,1 +1,65 @@
+const gnb = document.querySelector(".gnb");
+const loginBtn = document.querySelector(".login");
+const headerUserImg = document.querySelector(".header-user-img");
+const headerUserName = document.querySelector(".header-user-name");
+const headerUserFolder = document.querySelector(".header-user-folder");
+const sectionImgs = document.querySelectorAll(".section-img");
 
+const accessToken = localStorage.getItem("accessToken");
+if (accessToken) {
+  userData();
+  userFolderData();
+}
+
+async function userData() {
+  try {
+    const response = await fetch(
+      "https://bootcamp-api.codeit.kr/api/sample/user"
+    );
+    const userDataObj = await response.json();
+    loginBtn.classList.add("hide");
+    makeElement(userDataObj);
+  } catch {
+    console.log("error");
+  }
+}
+
+async function userFolderData() {
+  try {
+    const response = await fetch(
+      "https://bootcamp-api.codeit.kr/api/sample/folder"
+    );
+    const userDataObj = await response.json();
+    console.log(userDataObj);
+    makeHeaderUserData(userDataObj);
+    showImgs(userDataObj);
+  } catch {
+    console.log("error");
+  }
+}
+
+function makeElement(userDataObj) {
+  const a = document.createElement("a");
+  a.classList.add("user-profile");
+  const imgDiv = document.createElement("img");
+  imgDiv.src = userDataObj.profileImageSource;
+  const span = document.createElement("span");
+  span.innerText = userDataObj.email;
+  a.appendChild(imgDiv);
+  a.appendChild(span);
+  gnb.appendChild(a);
+  headerUserImg.src = userDataObj.profileImageSource;
+  headerUserName.innerText = `@${userDataObj.name}`;
+}
+
+function makeHeaderUserData(userFolderData) {
+  headerUserFolder.innerText = userFolderData.folder.name;
+}
+
+function showImgs(userFolderData) {
+  const folderLinks = userFolderData.folder.links;
+  console.log(folderLinks);
+  folderLinks.forEach((element, i) => {
+    sectionImgs[i].src = element.imageSource;
+  });
+}
